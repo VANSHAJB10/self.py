9ede911ca2101382183f4df59f0a135a68760a54
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-30 23:49:38 +0530

    Update Jinja2.py

--- Jinja2.py
+++ Jinja2.py
@@ -1,3 +1,8 @@
 # 1. Jinja2 Expression to use the variable `name` that generates the expected output. 
-
 The name is {{name}}
+
+# 2. Update the jinja2 expression to display the `name` in UPPERCASE
+The name is {{ name|upper }}
+
+# 3. to replace the word 'Bourne' in dialogue with 'Bond'
+{{ dialogue| replace ("Bourne", "Bond") }}
