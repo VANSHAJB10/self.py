412946140eb6af6b8b8c0bfe1a6f645f149cb71e
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-06-24 23:53:42 +0530

    Update Concepts.py

--- Concepts.py
+++ Concepts.py
@@ -1,5 +1,13 @@
+# Lists and dictionary
+
 Similar to Lists there are multiple ways of deleting elements from a dictionary.
 
 The pop() method removes the item with the specified key name:
 The del keyword removes the item with the specified key name and can also delete the whole dictionary
 The clear() method empties the dictionary:
+
+With the "for" loop we can execute a set of statements, once for each item in a list, tuple, string etc.
+EXAMPLE-- 
+color = ["Red","Blue","Green","Black"]
+for x in color:
+  print(x)
