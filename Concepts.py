af1c448c7b79fffe35c0694e6ebffd746fd8a42b
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-06-24 23:47:02 +0530

    Create Concepts.py

---
+++ Concepts.py
@@ -0,0 +1,5 @@
+Similar to Lists there are multiple ways of deleting elements from a dictionary.
+
+The pop() method removes the item with the specified key name:
+The del keyword removes the item with the specified key name and can also delete the whole dictionary
+The clear() method empties the dictionary:
